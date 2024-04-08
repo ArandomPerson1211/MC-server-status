a8f40579b9f9a331ba1ea70f081fd4ed9b882467
ArandomPerson1211 <isaacmolesini@gmail.com>
2024-04-08 05:08:01 +0100

    Released 1.0

--- MC server status.py
+++ MC server status.py
@@ -15,41 +15,31 @@ def menu():
 
 # Defines Fnc1
 def function1():
-    while True:
-        try:
+    try:
 
-            # Gets IP
-            user_input = input("Input valid server ip: ")
-            server = JavaServer.lookup(user_input)
-            status = server.status()
+        # Gets IP
+        user_input = input("Input valid server ip: ")
+        server = JavaServer.lookup(user_input)
+        status = server.status()
         
 
-          # Prints the amount of Players online \ Prints MOTD
-            print(colorama.Fore.CYAN, f"{user_input} has a max player count of {status.players.max}")
-            print(colorama.Fore.LIGHTGREEN_EX, f"{user_input} has {status.players.online} player(s) online and replied in {status.latency} ms")
+        # Prints the amount of Players online \ Prints MOTD
+        print(colorama.Fore.CYAN, f"{user_input} has a max player count of {status.players.max}")
+        print(colorama.Fore.LIGHTGREEN_EX, f"{user_input} has {status.players.online} player(s) online and replied in {status.latency} ms")
 
 
-            # Gets player(s) name
-            query = server.query()
-            print(colorama.Fore.LIGHTGREEN_EX, f"{user_input} has the following players online: {query.players.names}")
+        # Gets player(s) name
+        query = server.query()
+        print(colorama.Fore.LIGHTGREEN_EX, f"{user_input} has the following players online: {query.players.names}")
     
 
-        # Handles errors
-        except Exception as error:
-            print(colorama.Fore.LIGHTRED_EX, f"An error has ocurred: {error}")
-        if status.latency > 0:
-            print(colorama.Fore.YELLOW, "The inputted Server is still up either query in server.properties is disabled or the server is under too much load to respond.")
-            print(colorama.Fore.YELLOW, "Note: Most servers have query disabled", colorama.Fore.RESET)
+    # Handles errors
+    except Exception as error:
+        print(colorama.Fore.LIGHTRED_EX, f"An error has ocurred: {error}")
+    if status.latency > 0:
+        print(colorama.Fore.YELLOW, "The inputted Server is still up either query in server.properties is disabled or the server is under too much load to respond.")
+        print(colorama.Fore.YELLOW, "Note: Most servers have query disabled", colorama.Fore.RESET)
     
-        choice = input("Do you want to go back to the menu (1) or keep pinging the server every 5 seconds (2)? ")
-        if choice == "1":
-            break
-        elif choice == "2":
-            continue
-        else:
-            print("invald choice")
-            break
-
 
 
 # Defines Func2
