a2bb581ba4e3fbcf9288238c4f475d6c92b1b7c0
Kai Jane <kaijanedev@icloud.com>
2024-04-09 12:49:44 +0800

    Update .gitignore and MC server status.py

--- MC server status.py
+++ MC server status.py
@@ -1,81 +1,117 @@
 # Imports packages
-from mcstatus import JavaServer
-from mcstatus import BedrockServer
+from mcstatus import *
 import colorama
+
+
+# Initializes colorama
+class Colours:
+
+    RESET = colorama.Fore.RESET
+    GREEN = colorama.Fore.GREEN
+    RED = colorama.Fore.RED
+    CYAN = colorama.Fore.CYAN
+    YELLOW = colorama.Fore.YELLOW
+    MAGENTA = colorama.Fore.MAGENTA
+    ORANGE = '\033[38;5;214m'
+
+    L_GREEN = colorama.Fore.LIGHTGREEN_EX
+    L_RED = colorama.Fore.LIGHTRED_EX
+    L_CYAN = colorama.Fore.LIGHTCYAN_EX
+    L_YELLOW = colorama.Fore.LIGHTYELLOW_EX
+    L_MAGENTA = colorama.Fore.LIGHTMAGENTA_EX
 
 
 # Defines Menu
 def menu():
-    colorama.Fore.RESET
-    choice = input("Type Java for Java edition Servers or Bedrock for Bedrock edition server: ")
+    
+    print(Colours.CYAN, "1. Java")
+    print(Colours.CYAN, "2. Bedrock")
+    print(Colours.CYAN, "3. Exit", colorama.Fore.RESET)
+
+    choice = input("Choose a server type: ")
+
+    try:
+        choice = int(choice)
+        return choice
+    except ValueError:
+        pass
+    
+    if type(choice) == str:
+        choice = choice.lower()
 
     return choice
 
 
 # Defines Fnc1
-def function1():
-    try:
+def javaServer():
 
-        # Gets IP
-        user_input = input("Input valid server ip: ")
-        server = JavaServer.lookup(user_input)
-        status = server.status()
+    # Gets IP
+    user_input = input("Input valid server ip: ")
+    port = input("Input valid server port: ")
+    
+    try: 
         
+        server = JavaServer.lookup(f"{user_input}:{port}")
 
-        # Prints the amount of Players online \ Prints MOTD
-        print(colorama.Fore.CYAN, f"{user_input} has a max player count of {status.players.max}")
-        print(colorama.Fore.LIGHTGREEN_EX, f"{user_input} has {status.players.online} player(s) online and replied in {status.latency} ms")
+    except Exception as error:
+        print(Colours.L_RED, f"An error has ocurred: {error}")
+        return
+    
+    status = server.status()
 
+    
+    # Prints the amount of Players online 
+    print(Colours.CYAN, f"{user_input} has a max player count of {status.players.max}")
+    print(Colours.CYAN, f"{user_input} has {status.players.online} player(s) online and replied in {status.latency} ms")
+    
 
+    try:
         # Gets player(s) name
         query = server.query()
-        print(colorama.Fore.LIGHTGREEN_EX, f"{user_input} has the following players online: {query.players.names}")
-    
+        print(Colours.L_GREEN, "Players online: ", query.players.names)
 
-    # Handles errors
     except Exception as error:
-        print(colorama.Fore.LIGHTRED_EX, f"An error has ocurred: {error}")
-    if status.latency > 0:
-        print(colorama.Fore.YELLOW, "The inputted Server is still up either query in server.properties is disabled or the server is under too much load to respond.")
-        print(colorama.Fore.YELLOW, "Note: Most servers have query disabled", colorama.Fore.RESET)
-    
+        print(Colours.L_RED, f"An error has ocurred: {error}")
+        print(Colours.ORANGE, "This server does not query enabled in server.properties")
+
 
 # Defines Func2
-def function2():
+def bedrockServer():
 
     try:
         
         # Gets bedrock IP
-        bruser_input = input("Input valid server ip: ")
-        brserver = BedrockServer.lookup(bruser_input)
-        status = brserver.status()
+        BRuser_input = input("Input valid server ip: ")
+        BRserver = BedrockServer.lookup(BRuser_input)
+        status = BRserver.status()
 
 
-        print(f"{bruser_input} has a max player count of {status.players.max}")
-        print(colorama.Fore.LIGHTGREEN_EX, f"{bruser_input} has {status.players.online} player(s) online and replied in {status.latency} ms")
+        print(f"{BRuser_input} has a max player count of {status.players.max}")
+        print(Colours.CYAN, f"{BRuser_input} has {status.players.online} player(s) online and replied in {status.latency} ms")
         
 
-    except Exception as brerror:
-        print(colorama.Fore.LIGHTRED_EX, f"An error has ocurred: {brerror}")
+    except Exception as BRerror:
+        print(Colours.L_RED, f"An error has ocurred: {BRerror}")
 
 
 # Defines choices
 while True:
 
-
     choice = menu()
+
     match choice:
 
         # Func1
-        case "Java":
-            function1()
+        case 1 | "java":
+            javaServer()
 
-        
         # Func2
-        case "Bedrock":
-                function2()
+        case 2 | "bedrock":
+            bedrockServer()
 
-        
+        case 3 | "exit":
+            exit()
+
         # Invalid choice
         case _:
-            print(colorama.Fore.RED, "Invalid choice", colorama.Fore.RESET)
+            print(Colours.L_RED, "Invalid choice", Colours.RESET)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Just a simple Minecraft server status python project i'm working on
 Does Java and Bedrock servers
+
+You will need to install the dependencies you can do this with pip easily for example
+```zsh
+
+pip install -r requirement.txt
+
+```
